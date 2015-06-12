a516463a3b1e2f277016c57732fa976c70b31e41
Cliff Rodgers + Will Strinz <dev+kliph+wstrinz@bendyworks.com>
2015-06-12 15:49:20 -0500

    working ui-grid issues view

--- public/plugin_assets/gmail_issues/javascripts/index.js
+++ public/plugin_assets/gmail_issues/javascripts/index.js
@@ -1,14 +1,54 @@
 console.log('im alive');
 
-var issuesApp = angular.module('issuesApp', ['ngResource']);
+var issuesApp = angular.module('issuesApp', ['restangular', 'ui.grid']);
 
 // create the controller and inject Angular's $scope
-issuesApp.controller('mainController', ['$resource', function($resource) {
-  var ApiIssue = $resource('/issues/:issueId', {issueId: '@id'});
+issuesApp.controller('mainController', function($scope, Restangular) {
+  // var ApiIssue = $resource('/issues/:issueId.json', {issueId: '@id'});
 
-  this.issues = [{title: 'hello'}, {title: 'yep'}];
+  this.issues = [];
 
-  this.apiIssues = function() {
-    return ApiIssue.query();
+  this.apiIssues = [];
+  $scope.issueData = [];
+  $scope.gridOptions = {
+    enableFiltering: true,
+    columnDefs: [
+      {field: 'subject'},
+      {field: 'description'},
+      {field: 'priorityId'},
+      {field: 'authorId'},
+      {field: 'startDate'},
+      {field: 'doneRatio'}
+    ],
+    data: []
   };
-}]);
+
+  var cont = this;
+
+  Restangular.addResponseInterceptor(function(data, operation) {
+    var extractedData;
+    if (operation === 'getList') {
+      extractedData = data.issues;
+    } else {
+      extractedData = data;
+    }
+    return extractedData;
+  });
+
+  Restangular.all('issues.json').getList().then(function(issues) {
+    cont.apiIssues = issues;
+
+    $scope.gridOptions.data = _.map(issues, function(i) {
+      console.log(i);
+      return {
+        'subject': i.subject,
+        'description': i.description,
+        'priorityId': i.priority.name,
+        'authorId': i.author.name,
+        'startDate': i.start_date,
+        'doneRatio': i.done_ratio
+      };
+    });
+    console.log($scope.issueData);
+  });
+});
